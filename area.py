ceae439772b342fa9d86896c7b76da6666efbe00
sayanalokesh <lokesh.sayana@gmail.com>
2023-08-08 22:39:30 +0530

    modify the code

--- area.py
+++ area.py
@@ -11,7 +11,7 @@ if __name__ == "__main__": calculator = GeometryCalculator()
 # print(f"The area of the circle with radius {radius} ={calculator.calculate_circle_area(radius)}")
  
 # TODO: Implement the feature to calculate the area of a rectangle 
-length = 10
+length = 12
 width = 6
 
 print(f"The area of the rectangle with length {length} and width {width} = {calculator.calculate_rectangle_area(length, width)}")
